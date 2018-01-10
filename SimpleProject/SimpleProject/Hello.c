4a0f0a3f5b953f397e377b1e76571e6ee9c23122
혜진 김 <traceimp@outlook.kr>
2018-01-10 20:06:47 +0900

    SimpleProject(Part1 첫번째 프로젝트) 추가

---
+++ SimpleProject/SimpleProject/Hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World! \n");
+
+	// Page 39
+	// 문제 1
+	printf("홍길동\n홍 길 동\n홍  길  동\n");
+
+	// 문제 2
+	printf("이름: 홍길동\n");
+	printf("주소: 대한민국 한국시\n");
+	printf("전화번호: 010-1234-2345\n");
+}
