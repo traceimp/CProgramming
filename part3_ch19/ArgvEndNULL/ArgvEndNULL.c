cecdb84c1bd9194c43a92f0e54a0bf74386e82a2
혜진 김 <traceimp@outlook.kr>
2018-02-07 16:45:50 +0900

    180207 part3 > chapter 19 업데이트

---
+++ part3_ch19/ArgvEndNULL/ArgvEndNULL.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	printf("전달된 문자열의 수: %d \n", argc);
+
+	while (argv[i] != NULL)
+	{
+		printf("%d번째 문자열: %s \n", i+1, argv[i]);
+		i++;
+	}
+
+	/*for (i = 0; i < argc; i++)
+	{
+		printf("%d번째 문자열: %s \n", i + 1, argv[i]);
+	}*/
+
+	return 0;
+}
